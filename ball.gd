267a429207754462d869f65c87f7993f1e042ef4
Eric So <me@eric.so>
2025-07-14 15:43:53 -0700

    cleanup

--- ball.gd
+++ ball.gd
@@ -1,7 +1,6 @@
 extends RigidBody3D
 
 @export var move_force := 10.0
-const CAMERA_RIG_NAME := "CameraRig" # Match your actual node name
 var max_angular_speed := 10.0
 
 var camera_rig: Node3D
@@ -15,7 +14,7 @@ func _ready():
 func _physics_process(delta):
 	if camera_rig == null:
 		return
-
+	
 	var input_dir = Vector3.ZERO
 	if Input.is_action_pressed("move_forward"):
 		input_dir.z -= 1
@@ -25,20 +24,20 @@ func _physics_process(delta):
 		input_dir.x -= 1
 	if Input.is_action_pressed("move_right"):
 		input_dir.x += 1
-
+	
 	input_dir = input_dir.normalized()
 	if input_dir == Vector3.ZERO:
 		return
-
+	
 	var cam_basis = camera_rig.global_transform.basis
 	var forward = cam_basis.z
 	var right = cam_basis.x
-
+	
 	forward.y = 0
 	right.y = 0
 	forward = forward.normalized()
 	right = right.normalized()
-
+	
 	var move_vec = (forward * input_dir.z + right * input_dir.x).normalized()
 	var torque = Vector3(move_vec.z, 0, -move_vec.x) * move_force
 	apply_torque_impulse(torque)
